929d2ba47cf40f30071bbf3c589c20ca9cdcf971
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-09-01 11:51:20 +0300

    Interface için bir örnek oluşturuldu

---
+++ Siniflar/FrmInterface.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Siniflar
+{
+    public partial class FrmInterface : Form
+    {
+        public interface IPerson
+        {
+            int Id { get; set; }
+            string City { get; set; }
+        }
+
+        public class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string City { get; set; }
+            public int MyProperty { get; set; }
+        }
+
+        public FrmInterface()
+        {
+            InitializeComponent();
+
+            //Örnek 1
+            IPerson person = new Customer();
+            person.Id = 4; //burada myPropery erişemeyiz cunkü biz direk person sınıfına ulastık
+
+            Customer customer = new Customer();
+            customer.Id = 3; //burada hem properylere hemde implante ulaşabiliri kalıtım gibi
+        }
+    }
+}
