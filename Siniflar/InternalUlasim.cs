3d59033451dff28ab24dd066d9134ccaa3bfa6fe
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-09-01 14:22:11 +0300

    Bir clasın defaultu her zaman interaldır. Bağlı bulundugu proje(assambely) içersinde referens ihtiyacı olmadan kullanılır bu nedenle InternalClass diye bir sınıf oluşturdum ornek olarak ona InternalUlasımdan erişim sağladım

---
+++ Siniflar/InternalUlasim.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siniflar
+{
+    class InternalUlasim
+    {
+        public void Add()
+         
+        {   InternalClass ınternalUlaşalım = new InternalClass();
+            ınternalUlaşalım.Name = "Ulaşım başarılı";
+        }
+    }
+}
